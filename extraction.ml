7295bc38eef1619a3adb218a3e57e691320c305b
Durant Florian <Phlau@MacBook-Air-de-Durant.local>
2018-11-30 11:51:25 +0100

    First com

--- extraction.ml
+++ extraction.ml
@@ -6,3 +6,13 @@ let ouverture = fun file ->
   with exc ->
     Printf.printf "%s ne s'ouvre pas en lecture\n" file;
      raise exc;;
+
+let read_line = fun ligne ->
+  match Str.split (Str.regexp " ") ligne with (* or bounded_split ? *)
+    id :: t_i :: t_f :: gates  ->
+    |_ -> failwith "read_line: unexpected format";;
+
+
+let extract = fun file ->
+  let canal_entree = ouverture file in
+  let rec extract_rec = fun canal ->
