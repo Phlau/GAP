6a355a487e7ee92b3fe2d00f50b7bc9985329811
Durant Florian <Phlau@MacBook-Air-de-Durant.local>
2018-12-03 10:34:43 +0100

    2nd commit

--- extraction.ml
+++ extraction.ml
@@ -1,18 +1,18 @@
-
+(* AJOUTER LE str.cma DANS LA COMPIL *)
 
 let ouverture = fun file ->
   try
     open_in file
   with exc ->
     Printf.printf "%s ne s'ouvre pas en lecture\n" file;
-     raise exc;;
+    raise exc;;
 
 let read_line = fun ligne ->
   match Str.split (Str.regexp " ") ligne with (* or bounded_split ? *)
-    id :: t_i :: t_f :: gates  ->
+    id :: t_i :: t_f :: gates  -> (* Ajouter la structure *)
     |_ -> failwith "read_line: unexpected format";;
 
 
 let extract = fun file ->
   let canal_entree = ouverture file in
-  let rec extract_rec = fun canal ->
+  let rec extract_rec = fun canal -> (* Essayer de boucler rec jusqu'à la fin du fichier, penser a virer les lignes du debut *)
