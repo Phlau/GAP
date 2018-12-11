e356ebe3f3ec1ec4ffc948eef6d114c9e360e147
Durant Florian <Phlau@MacBook-Air-de-Durant.local>
2018-12-11 15:57:36 +0100

    Chgt list en array

--- classes.ml
+++ classes.ml
@@ -1,13 +1,13 @@
 
 type avion = {
-    mutable id_avion : int;
+    id_avion : int;
   h_arrivee : int; (*Temps en minutes pour le moment*)
   h_depart : int;
-  mutable gate_candidate : gate list;
-  mutable gate_attribuee : gate}
+  gate_candidate : gate array
+  }
 and gate = {
     id_gate : int;
-  (*avion_attribue : avion list;*)
+  avion_attribue : avion list;
   mutable delta : int;
     mutable conflits : int};; (*Nombre de conflits par Gate*)
 
@@ -15,38 +15,25 @@ type changement =
     Deplacement of avion*gate
   |Switch_2 of avion*avion (*switch=deux deplacements*)
   |Switch_3 of avion*avion*avion;;
-  
 
-let gate1 = {id_gate=1;delta=0;conflits=0};;
-let gate2 = {id_gate=2;delta=0;conflits=0};;
-
-type solution = avion list;;
-
-let avion1 = { id_avion = 1; h_arrivee=10; h_depart = 20; gate_candidate = [gate1];gate_attribuee = gate1 };;
-let avion2 = { id_avion = 2; h_arrivee=10; h_depart = 20; gate_candidate = [gate1];gate_attribuee = gate2 };;
+type solution = {
+    plane_to_gate : int array; (*ieme élément correspond à l'avion i associé à la gate*)
+    gates : gate array};;
+(*
+let avion1 = { id_avion = 1; h_arrivee=10; h_depart = 20; gate_candidate = [|gate1|]}
+let avion2 = { id_avion = 2; h_arrivee=10; h_depart = 20; gate_candidate = [|gate1|]};;
 avion1.id_avion<-2;;
 
-let deplacement_gate = fun avion gate ->
-  avion.gate_attribuee<-gate;;
+let gate1 = {id_gate=1;avion_attribue=avion1;delta=0;conflits=0};;
+let gate2 = {id_gate=2;avion_attribue=avion2;delta=0;conflits=0};;
+*)
 
-let switch_gate_2 = fun avion1 avion2 ->
-  let gate = avion1.gate_attribuee in
-  avion1.gate_attribuee<-avion2.gate_attribuee;
-  avion2.gate_attribuee<-gate;;
-
-let switch_gate_3 = fun avion1 avion2 avion3 ->
-  let gate1 = avion1.gate_attribuee in
-  let gate2= avion2.gate_attribuee in
-  avion1.gate_attribuee<-avion3.gate_attribuee;
-  avion2.gate_attribuee<-gate1;
-  avion3.gate_attribuee<-gate2;;
-         
-  
+(*
 let changement_gate = fun change ->
   match change with
     Deplacement (avion1,gate1) -> deplacement_gate avion1 gate1
   |Switch_2 (avion1,avion2) -> switch_gate_2 avion1 avion2
-  |Switch_3 (avion1,avion2,avion3) -> switch_gate_3 avion1 avion2 avion3;;
+  |Switch_3 (avion1,avion2,avion3) -> switch_gate_3 avion1 avion2 avion3;;*)
 
 (*switch_gate avion1 avion2;;*)
 
@@ -59,18 +46,40 @@ Printf.printf "%d\n" avion2.gate_attribuee.id_gate;;*)
   Printf.printf "%d\n" avion1.gate_attribuee.id_gate;;*)
 
 let init_gate = fun id_gate1 ->
-  let gate = { id_gate = id_gate1; delta=0; conflits=0 } in
+  let gate = { id_gate = id_gate1; avion_attribue=[]; delta=0; conflits=0 } in
   gate;; (*on connait pas delta et conflits au départ*)
 
 let init_avion = fun id_avion1 h_arrivee1 h_depart1 gate_candidate1 ->
-  let avion = { id_avion = id_avion1; h_arrivee=h_arrivee1; h_depart = h_depart1; gate_candidate = gate_candidate1 ; gate_attribuee = (init_gate 0) } in
+  let avion = { id_avion = id_avion1; h_arrivee=h_arrivee1; h_depart = h_depart1; gate_candidate = gate_candidate1 } in
   avion;;(*on a pas accès à gate_attribuee*)
 
 
 let is_tabu = fun change tabu_list ->
   List.mem change tabu_list;; (*renvoie true si change est dans tabu_list*)
- 
-  
-  
-  
 
+let nb_gate = 10;;
+
+
+
+
+
+(*let ordonner_sol = fun sol ->
+  let plane_by_gate = Array.make nb_gate [] in (*10 deviendra nb_gate de florian*)
+  for i=0 to List.length sol do
+    let avion_i = List.nth sol i in
+    plane_by_gate.(avion_i.gate_attribuee.id_gate)<-List.append (plane_by_gate.(avion_i.gate_attribuee.id_gate)) [avion_i];
+  done;
+  for j=0 to nb_gate do
+    plane_by_gate.(j) = List.sort (fun avion1 avion2 -> if avion1.h_arrivee<avion2.h_arrivee then 1 else -1) plane_by_gate.(j);
+    done;
+  plane_by_gate;;*)
+
+(*
+let fonction_objectif = fun obj_solution -> (*solution est un objet de type solution*)
+  let somme_delta=0 in
+  let conflits=0 in
+  for i=0 to Array.length obj_solution.gates do
+    somme_delta = somme_delta + fst (delta obj_solution.gates.(i).avion_attribue);
+    conflits = conflits + snd (delta obj_solution.gates.(i).avion_attribue);
+    done;
+    (somme_delta,conflits);;*)
--- extraction.ml
+++ extraction.ml
@@ -1,8 +1,8 @@
 (* AJOUTER LE str.cma DANS LA COMPIL *)
 (* Et surement classes.ml *)
 
-let NB_GATE = ref 0;;
-let NB_AVION = ref 0;;
+let _NB_GATE = ref 0;;
+let _NB_AVION = ref 0;;
 
 let ouverture = fun file ->
   try
@@ -16,15 +16,16 @@ let heure_to_min_int = fun t ->
   let t_fin = int_of_string (List.hd t_split)*60 + int_of_string (List.nth t_split 1) in
   t_fin;;
 
-let read_line = fun ligne -> (* Gerer id_avion qui est au format "#01:" *)
+let read_line = fun ligne ->
   match Str.split (Str.regexp " ") ligne with (* String.split_on_char pas possible car ocaml 4.02.1 *)
     |id :: t_i :: t_f :: gates  -> let gates_list = List.map Classes.init_gate (List.map int_of_string gates) in
-            let last_gate = List.nth gates_list ((List.length gates_list)-1) in
-            if !nb_gate < last_gate.id_gate then nb_gate := last_gate.id_gate;
-            NB_AVION := !NB_AVION + 1;
+            let gates_array = Array.of_list gates_list in
+            let last_gate = gates_array.((Array.length gates_array)-1) in
+            if !_NB_GATE < last_gate.id_gate then _NB_GATE := last_gate.id_gate;
+            _NB_AVION := !_NB_AVION + 1;
             let n = String.length id in
             let id_av = int_of_string (String.sub id 1 (n-2)) in
-            let avion = Classes.init_avion id_av (heure_to_min_int t_i) (heure_to_min_int t_f) gates_list in
+            let avion = Classes.init_avion id_av (heure_to_min_int t_i) (heure_to_min_int t_f) gates_array in
             avion
     | _ -> failwith "read_line: unexpected format";;
 
@@ -38,8 +39,8 @@ let extract = fun file ->
   try
     while true; do
       lines := read_line (input_line canal_entree) :: !lines
-    done; !lines
+    done; Array.of_list !lines
   with End_of_file ->
     close_in canal_entree;
-    let avions = List.rev !lines in
+    let avions = Array.of_list (List.rev !lines) in
     avions;;
