dbd423b125e23ee9e25e78e7ae28b30dbcc6d214
Durant Florian <Phlau@MacBook-Air-de-Durant.local>
2018-12-12 11:51:56 +0100

    Graph begin to work

--- graph.ml
+++ graph.ml
@@ -1,5 +1,15 @@
-(* Graphics pas dispo *)
-Graphics.open_graph "foo:0";
-Graphics.set_color Graphics.blue;
-Graphics.fill_rect 0 0 10 30;
-Graphics.close_graph;
+(* Attendre le clic pour enlever la fenetre *)
+
+let x_size_tot = 1200;;
+let y_size_tot = 700;;
+
+Graphics.open_graph " 1200x700";;
+Graphics.set_color Graphics.blue;;
+Graphics.fill_rect 0 0 100 300;;
+
+Unix.sleep 5;;
+
+Graphics.close_graph;;
+(* Heure en taille pour integrer direct,
+   avec la taille global et heure debut/fin  pour savoir (le ratio) et la position *)
+let time_to_size = fun h_dep h_fin x_size y_size ->
